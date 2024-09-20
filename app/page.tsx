cc65f49d03372edf5de0992ea8eda50274a2e2c7
Lelisayohanes <lelisayohanes1991@gmail.com>
2024-09-20 08:28:04 +0300

    mistake made

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       <VideoBackground />
       <div className="relative z-20 flex flex-col justify-start  h-full text-white text-center">
         <Navigation />
-        <div className='flex flex-col justify-center items-center text-gray-950' >
-          <h1 className="text-6xl font-bold text-center  mt-20">HANI BEAUTY ACADEMY</h1>
+        <div className='flex flex-col justify-center items-center text-gray-950 bg-pink-50 lg:transparent lg:opacity-30 h-screen' >
+          <h1 className="sm:text-2xl  md:text-4xl lg:text-6xl font-bold text-center  mt-20">HANI BEAUTY ACADEMY</h1>
           <p className="mt-4 text-lg font-semibold">Let Your Talent Flourish</p><br></br>
           <p className="text-2xl  font-semibold">Let Your Beauty Shine.</p>
         </div>
